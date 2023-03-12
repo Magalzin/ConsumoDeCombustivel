8cfc4df5c8dfd38c5ad9b5d42f09eb01f1468404
Magalzin <matheusschorro@gmail.com>
2023-03-12 14:14:18 -0300

    Primeiro Commit para meu projeto

---
+++ Teste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quilometragem:");
+            int Km = int.Parse(Console.ReadLine());
+            Console.Write("Digite de litros:");
+            double Litros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double Consumo = Km / Litros;
+
+            var result = $"O consumo medio foi {Consumo:#.000}";
+
+            Console.WriteLine(result);
+            
+            // teste
+            //Console.WriteLine(Consumo.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+} 
